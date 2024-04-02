d279da835a69cc222cc0809d65ebeeb3315797fe
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-02 02:32:43 +0100

    initial commit

---
+++ 01/PublisherConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PublisherData;
+using PublisherDomain;
+
+using (PubCOntext context = new PubCOntext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+AddAuthor();
+void GetAuthors()
+{
+    using var context = new PubCOntext();
+    var authors = context.Authors.ToList();
+
+    foreach(var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
+
+void AddAuthor()
+{
+    Author author = new() { FirstName = "Julie", LastName = "Lerman" };
+    PubCOntext pubCOntext = new PubCOntext();
+
+    pubCOntext.Authors.Add(author);
+    pubCOntext.SaveChanges();
+}
---
+++ 01/PublisherData/PubCOntext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubCOntext:DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=433571323T5SL; Initial Catalog = PubDatabase; User Id = tmaxnoda; Password=tmaxnoda123@");
+        }
+    }
+}
