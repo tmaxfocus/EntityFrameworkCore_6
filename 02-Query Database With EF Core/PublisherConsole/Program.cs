18566d9f4e12922e9582e9d5e6b43b92dc59e180
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-02 07:54:02 +0100

    02-Querying Database with EFcore

---
+++ 02-Query Database With EF Core/PublisherConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using PublisherData;
+using PublisherDomain;
+
+
+
+GetAuthors();
+
+void GetAuthors()
+{
+    using var context = new PubCOntext();
+    var authors = context.Authors.ToList();
+
+    foreach(var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
+
+void GetAuthorsWithBooks()
+{
+    using var context = new PubCOntext();
+    var authors = context.Authors.Include(x => x.Books).ToList();
+
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach(var book in author.Books)
+        {
+            Console.WriteLine(book.Title + " " + book.PublishDate);
+        }
+    }
+}
+
+void AddAuthor()
+{
+    Author author = new() { FirstName = "Julie", LastName = "Lerman" };
+    PubCOntext pubCOntext = new PubCOntext();
+
+    pubCOntext.Authors.Add(author);
+    pubCOntext.SaveChanges();
+}
+
+void AddAuthorWithBook()
+{
+    Author author = new() { FirstName = "Julie", LastName = "Lermax" };
+    author.Books.Add(new Book { Title = "Programing Entity Framework", PublishDate = new DateTime(2009, 1, 1) });
+
+    author.Books.Add(new Book { Title = "Programing Entity Framework 2nd Ed", PublishDate = new DateTime(2010, 1, 1) });
+    PubCOntext pubCOntext = new PubCOntext();
+
+    pubCOntext.Authors.Add(author);
+    pubCOntext.SaveChanges();
+}
