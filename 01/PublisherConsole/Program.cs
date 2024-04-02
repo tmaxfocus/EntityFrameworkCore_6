b4ac45ade9f066041f083eb2088b2b52e0d7ca7f
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-02 07:49:28 +0100

    Finished on unsing entity framework

--- 01/PublisherConsole/Program.cs
+++ 01/PublisherConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
 using PublisherData;
 using PublisherDomain;
 
@@ -7,8 +8,11 @@ using (PubCOntext context = new PubCOntext())
     context.Database.EnsureCreated();
 }
 
-GetAuthors();
-AddAuthor();
+//GetAuthors();
+//AddAuthor();
+//GetAuthors();
+//AddAuthorWithBook();
+GetAuthorsWithBooks();
 void GetAuthors()
 {
     using var context = new PubCOntext();
@@ -20,9 +24,36 @@ void GetAuthors()
     }
 }
 
+void GetAuthorsWithBooks()
+{
+    using var context = new PubCOntext();
+    var authors = context.Authors.Include(x => x.Books).ToList();
+
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach(var book in author.Books)
+        {
+            Console.WriteLine(book.Title + " " + book.PublishDate);
+        }
+    }
+}
+
 void AddAuthor()
 {
     Author author = new() { FirstName = "Julie", LastName = "Lerman" };
+    PubCOntext pubCOntext = new PubCOntext();
+
+    pubCOntext.Authors.Add(author);
+    pubCOntext.SaveChanges();
+}
+
+void AddAuthorWithBook()
+{
+    Author author = new() { FirstName = "Julie", LastName = "Lermax" };
+    author.Books.Add(new Book { Title = "Programing Entity Framework", PublishDate = new DateTime(2009, 1, 1) });
+
+    author.Books.Add(new Book { Title = "Programing Entity Framework 2nd Ed", PublishDate = new DateTime(2010, 1, 1) });
     PubCOntext pubCOntext = new PubCOntext();
 
     pubCOntext.Authors.Add(author);
